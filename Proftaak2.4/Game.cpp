b39d3311c707c4aaec86505bf82bb494e4922afa
Dion <dionvanderlinden@live.nl>
2019-05-20 10:22:15 +0200

    [Added] Basis funties engine
    Draw, update, key en mouse events

--- Proftaak2.4/Game.cpp
+++ Proftaak2.4/Game.cpp
@@ -1,32 +1,117 @@
 #include "Game.hpp"
+#include <list>
+#include <iostream>
+#include <GL\freeglut.h>
 
-int main(int argc, char** argv)
+#include "Camera.hpp"
+#include "GameObject.h"
+#include "CubeComponent.h"
+
+namespace Game
 {
-	Game().run();
-	return 0;
-}
+	int windowWidth, windowHeight;
+	bool keys[256];
 
-Game::Game()
-{
+	Camera camera;
+	std::list<GameObject*> objects;
+	//GameObject* player;
+	//CameraComponent* camComponent;
+	void loadContent()
+	{
+		ZeroMemory(keys, sizeof(keys));
+		camera = Camera(0, -4, 0, 0);
 
-}
+		GameObject* o = new GameObject();
+		o->addComponent(new CubeComponent(0.5));
 
+		o->position = Vec3f(0, 4, 0);
 
-Game::~Game()
-{
-}
+		objects.push_back(o);
+	}
+	void update(float deltaTime)
+	{
+		for (const auto& o : objects)
+			o->update(deltaTime);
+	}
+	void draw()
+	{
+		//glRotatef(camera.rotX, 1, 0, 0);
+		//glRotatef(camera.rotY, 0, 1, 0);
+		//glTranslatef(camera.posX, 0, camera.posY);
 
-void Game::update(float elapsedTime)
-{
+		// Floor
+		glColor3f(0.1f, 1.0f, 0.2f);
 
-}
+		glBegin(GL_QUADS);
+		glVertex3f(-15, -1, -15);
+		glVertex3f(15, -1, -15);
+		glVertex3f(15, -1, 15);
+		glVertex3f(-15, -1, 15);
+		glEnd();
+		glColor3f(0.5f, 0.8f, 0.2f);
+		for (const auto& o : objects)
+			o->draw();
+		//glBegin(GL_QUADS);
+		//glVertex3f(-15, -1, -15);
+		//glVertex3f(15, -1, -15);
+		//glVertex3f(15, -1, 15);
+		//glVertex3f(-15, -1, 15);
+		//glEnd();
+	}
 
-void Game::draw()
-{
+	void onKey(Key key)
+	{
+		keys[key] = true;
+	}
 
-}
+	void onKeyUp(Key key)
+	{
+		keys[key] = false;
 
-void Game::loadContent()
-{
+		switch (key)
+		{
+		case VK_ESCAPE:
+			glutLeaveMainLoop();
+			break;
+		default:
+			break;
+		}
+	}
 
+	void onMouseMove(int x, int y)
+	{
+		/*static bool justMoved = false;
+
+		if (justMoved)
+		{
+			justMoved = false;
+			return;
+		}
+		int dx = x - windowWidth / 2;
+		int dy = y - windowHeight / 2;
+		if ((dx != 0 || dy != 0) && abs(dx) < 400 && abs(dy) < 400)
+		{
+			camera.rotY += dx / 10.0f;
+			camera.rotX += dy / 10.0f;
+			glutWarpPointer(windowWidth / 2, windowHeight / 2);
+			justMoved = true;
+		}*/
+	}
+
+	void onResize(int w, int h)
+	{
+		windowWidth = w;
+		windowHeight = h;
+	}
+
+	void onClose()
+	{
+		std::cout << "Cleaning up game\n";
+
+		/*for (auto& o : objects)
+			delete o;
+		objects.clear();*/
+
+		std::cout << "Closing game.\n";
+	}
 }
--- Proftaak2.4/Main.cpp
+++ Proftaak2.4/Main.cpp
@@ -1,4 +1,127 @@
+/*
+	Engine functionality
+*/
+#include <iostream>
 #include <GL\freeglut.h>
+#include "Globals.hpp"
+#include "Game.hpp"
+
+#define APP_NAME "Game"
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 800
+
+
+int windowWidth;
+int windowHeight;
+
+void reshape(int w, int h)
+{
+	windowWidth = w;
+	windowHeight = h;
+	glViewport(0, 0, w, h);
+}
+
+void keyboard(unsigned char key, int x, int  y)
+{
+	Game::onKey(key);
+}
+
+void keyboardup(unsigned char key, int x, int y)
+{
+	Game::onKeyUp(key);
+}
+
+void mousePassiveMotion(int x, int y)
+{
+	Game::onMouseMove(x, y);
+}
+
+void display()
+{
+	glClearColor(0.9f, 0.5f, 0.5f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(90.0f, windowWidth / (float)windowHeight, 0.1f, 50.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	Game::draw();
+
+	glutSwapBuffers();
+}
+
+int lastTime = 0;
+void idle()
+{
+	int currentTime = glutGet(GLUT_ELAPSED_TIME);
+	float deltaTime = (currentTime - lastTime) / 1000.0f;
+	lastTime = currentTime;
+
+	Game::update(deltaTime);
+	// update
+
+	glutPostRedisplay();
+}
+
+bool initGlut(int argc, char** argv)
+{
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(windowWidth, windowHeight);
+	glutInit(&argc, argv);
+	glutCreateWindow(APP_NAME);
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
+	glutKeyboardUpFunc(keyboardup);
+	glutPassiveMotionFunc(mousePassiveMotion);
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_CONTINUE_EXECUTION);
+
+	return true;
+}
+
+bool initOpenGL()
+{
+	glEnable(GL_DEPTH_TEST);
+
+	return true;
+}
+int main(int argc, char** argv)
+{
+	windowWidth = WINDOW_WIDTH;
+	windowHeight = WINDOW_HEIGHT;
+
+	if (initGlut(argc, argv))
+		std::cout << "Succesfully initialized glut.\n";
+	else
+	{
+		std::cout << "Failed initializing glut.\n";
+		return GLUT_INIT_FAILED;
+	}
+
+	if (initOpenGL())
+		std::cout << "Succesfully initialized OpenGL.\n";
+	else
+	{
+		std::cout << "Failed initializing OpenGL.\n";
+		return OPENGL_INIT_FAILED;
+	}
+
+	// Start loading in game content.
+	Game::loadContent();
+
+	Game::onResize(windowWidth, windowHeight);
+	// Start game.
+	glutMainLoop();
+
+	Game::onClose();
+
+	return EXIT_SUCCESS;
+}
+
+/* #include <GL\freeglut.h>
 #include <opencv2\highgui\highgui.hpp>
 
 #include <math.h>
@@ -131,6 +254,5 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
-
-
+*/
 
