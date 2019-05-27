83e4b3b7033b767d5f97df0cbd9ce3580f03ef4a
Olaf van Wijngaarden <odwijnga@avans.nl>
2019-05-27 11:46:31 +0200

    [Edit] stage sizes

--- Proftaak2.4/StageComponent.cpp
+++ Proftaak2.4/StageComponent.cpp
@@ -24,45 +24,26 @@ void StageComponent::draw()
 	glBegin(GL_QUADS);
 	glColor4f(1, 1, 1, 1);
 	glTexCoord2f(0, 0);
-	glVertex3f(-3, -2, 2.5);
+	glVertex3f(-3, -2, -2.5);
 	glTexCoord2f(0, 1);
 	glVertex3f(-3, -2, 100);
 	glTexCoord2f(1, 1);
 	glVertex3f(-3, 4, 100);
 	glTexCoord2f(1, 0);
-	glVertex3f(-3, 4, 2.5);
+	glVertex3f(-3, 4, -2.5);
 	glDisable(GL_TEXTURE_BINDING_2D);
-	glEnd();
-
-
-	//START WALL RIGHT
-	glBegin(GL_QUADS);
-	glColor3f(0, 1, 0);
-	glVertex3f(-2.5, -2, 2.49);
-	glVertex3f(-3.5, -2, 2.49);
-	glVertex3f(-3.5, 4, 2.49);
-	glVertex3f(-2.5, 4, 2.49);
 	glEnd();
 
 	//LEFT WALL
 	glBegin(GL_QUADS);
 	glColor4f(1, 1, 1, 1);
 	glEnable(GL_TEXTURE_BINDING_2D);
-	glVertex3f(3, -2, 2.5);
+	glVertex3f(3, -2, -2.5);
 	glVertex3f(3, -2, 100);
 	glVertex3f(3, 4, 100);
-	glVertex3f(3, 4, 2.5);
+	glVertex3f(3, 4, -2.5);
 	glEnd();
 	glDisable(GL_TEXTURE_BINDING_2D);
-
-	//START WALL LEFT
-	glBegin(GL_QUADS);
-	glColor3f(0, 1, 0);
-	glVertex3f(2.5, -2, 2.49);
-	glVertex3f(3.5, -2, 2.49);
-	glVertex3f(3.5, 4, 2.49);
-	glVertex3f(2.5, 4, 2.49);
-	glEnd();
 
 	//BASE PLATFORM
 	glBegin(GL_QUADS);
