adf8c90e800cf541250c15106b133425e28535cf
Ruben <rwoldhui@avans.nl>
2019-06-17 14:59:00 +0200

    [Changed]tracker to 2 colors

--- Proftaak2.4/Tracker.cpp
+++ Proftaak2.4/Tracker.cpp
@@ -1,11 +1,11 @@
 #include "Tracker.hpp"
-#include <opencv2/highgui.hpp>
 #include <iostream>
 #include <opencv2/tracking/tracker.hpp>
 #include <thread>
 
 #define camera_width 1240
 #define camera_height 720
+const auto element = getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(4, 4));
 
 //compareContourAreas
 //It returns a True when the i is smaller than j and it returns false when i is bigger than 
@@ -18,7 +18,7 @@ bool compareContourAreas(std::vector<cv::Point> contour1, std::vector<cv::Point>
 	return (i < j);
 }
 
-float map(float input, float srcRangeLow, float srcRangeHigh, float dstRangeLow, float dstRangeHigh)
+inline float map(float input, float srcRangeLow, float srcRangeHigh, float dstRangeLow, float dstRangeHigh)
 {
 	return dstRangeLow + (input - srcRangeLow) * (dstRangeHigh - dstRangeLow) / (srcRangeHigh - srcRangeLow);
 }
@@ -89,7 +89,7 @@ void HandTracker::trackByColor(const std::function<void(std::array<hand, HANDS_A
 
 			std::sort(contours.begin(), contours.end(), compareContourAreas);
 			//Find the contours of the blue objects.
-			std::vector<cv::Moments> mu(contours.size() );
+			std::vector<cv::Moments> mu(contours.size());
 
 			for (auto i = 0; i < mu.size(); i++)
 			{
@@ -144,58 +144,126 @@ void HandTracker::trackByColor(const std::function<void(std::array<hand, HANDS_A
 	}
 }
 
-//trackByTracker
-//It calls for a webcam frame
-//It tracks the hands of the user
-//Param @ callback: it calls the callback with an array of hands
-void HandTracker::trackByTracker(const std::function<void(std::array<hand, 2>)>& callback)
+void findCentroid(std::vector<cv::Mat>& contours, float* x, float* y)
 {
-	cv::UMat raw, scaled;
+	if (!contours.empty())
+	{
+		std::sort(contours.begin(), contours.end(), compareContourAreas);
+		cv::Moments mu = cv::moments(contours[0], false);
+		cv::Point2f mc = cv::Point2f(mu.m10 / mu.m00, mu.m01 / mu.m00);
+		*x = map(mc.x, 0, camera_width, -1, 1);
+		*y = -map(mc.y, 0, camera_height, -1, 1);
+	}
+}
 
-	auto* trackers = new cv::MultiTracker();
+void HandTracker::resize(int, int)
+{
+}
+
+std::vector<cv::Mat> blue_contours, green_contours;
+std::vector<cv::Vec4i> blue_hierarchy, green_hierarchy;
+
+void HandTracker::track(const std::function<void(std::array<hand, HANDS_AMOUNT>)>& callback)
+{
+	hand left = {0, 0, 0};
+	hand right = {1, 0, 0};
+
 	cv::useOpenVX();
 
-	cv::VideoCapture vcap(0);
-
-
-	std::vector<cv::Rect2d> boundingBoxes;
-
+	cv::UMat frame, scaled_frame, flipped_frame, blurred, hsv;
+	cv::VideoCapture vcap(1);
 	for (;;)
 	{
-		boundingBoxes.clear();
-		if (vcap.read(raw))
+		if (vcap.read(frame))
 		{
-			cv::resize(raw, scaled, cv::Size(640, 480));
+			//-------PRE-PROCESSING--------//
+			cv::resize(frame, scaled_frame, cv::Size(camera_width, camera_height));
+			//Resize the image to a easier resolution.
+			cv::flip(scaled_frame, flipped_frame, +1);
+			cv::GaussianBlur(flipped_frame, blurred, cv::Size(13, 13), 0); //Blur the image in order to remove noise.
+			cv::cvtColor(blurred, hsv, CV_BGR2HSV); //Convert the blurred image to HSV.
 
-			trackers->update(scaled, boundingBoxes);
-			for (auto box : boundingBoxes)
+			trackBlue(hsv, &left);
+			trackGreen(hsv, &right);
+
+			//hands_.push_back(&left);
+			//hands_.push_back(&right);
+			hands[0] = left;
+			hands[1] = right;
+			callback(hands);
+
+
+			if (TRACKER_DEBUG)
 			{
-				cv::rectangle(raw, cv::Point(box.x, box.y), cv::Point(box.x + box.width, box.y + box.height),
-				              cv::Scalar(0, 255, 0));
+				for (auto i = 0; i < green_contours.size(); i++)
+				{
+					cv::Scalar color = cv::Scalar(0, 0, 255); // B G R values
+					drawContours(flipped_frame, green_contours, i, color, 2, 8, green_hierarchy, 0, cv::Point());
+					//circle(flipped_frame, mc[i], 4, color, -1, 8, 0);
+				}
+				for (auto i = 0; i < blue_contours.size(); i++)
+				{
+					cv::Scalar color = cv::Scalar(0, 0, 255); // B G R values
+					drawContours(flipped_frame, blue_contours, i, color, 2, 8, blue_hierarchy, 0, cv::Point());
+					//circle(flipped_frame, mc[i], 4, color, -1, 8, 0);
+				}
+				cv::imshow("Contours", flipped_frame); //Finally show the image with the contours
 			}
-			cv::imshow("Raw", raw); //Finally show the image with the contours
 		}
 		else
 		{
 			std::cout << "couldn't read frame" << std::endl;
 			break;
 		}
-
-
-		auto key = cv::waitKey(1);
-		if (key == 27)
+		if (cv::waitKey(1) == 27)
 		{
 			break;
 		}
-		else if (key == 32)
-		{
-			auto box = cv::selectROI("Raw", raw);
-			auto tracker = cv::TrackerKCF::create();
-			trackers->add(tracker, raw, box);
-		}
 	}
+}
 
-	delete trackers;
+
+void HandTracker::trackBlue(cv::UMat& hsv, hand* hand)
+{
+	blue_contours.clear();
+	blue_hierarchy.clear();
+	const auto blue_lower = cv::Scalar(78, 102, 77);
+	const auto blue_upper = cv::Scalar(102, 255, 255);
+	cv::UMat blue_mask;
+
+
+	cv::inRange(hsv, blue_lower, blue_upper, blue_mask); //Create a binary output with only the blue's.
+
+	//Erode & Dilate to remove smaller objects
+	cv::erode(blue_mask, blue_mask, element, cv::Point(-1, -1), 4);
+	cv::dilate(blue_mask, blue_mask, element, cv::Point(-1, -1), 4);
+
+	//-------OBJECT-DETECTION------//
+	cv::findContours(blue_mask, blue_contours, blue_hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+	findCentroid(blue_contours, &hand->x, &hand->y);
+}
+
+void HandTracker::trackGreen(cv::UMat& hsv, hand* hand)
+{
+	green_contours.clear();
+	green_hierarchy.clear();
+	int color_sensitivity = 15;
+	const auto green_lower = cv::Scalar(60 - color_sensitivity, 100, 100);
+	const auto green_upper = cv::Scalar(60 + color_sensitivity, 255, 255);
+
+	cv::UMat green_mask;
+
+
+	cv::inRange(hsv, green_lower, green_upper, green_mask); //Create a binary output with only the blue's.
+
+	//Erode & Dilate to remove smaller objects
+	cv::erode(green_mask, green_mask, element, cv::Point(-1, -1), 4);
+	cv::dilate(green_mask, green_mask, element, cv::Point(-1, -1), 4);
+
+	//-------OBJECT-DETECTION------//
+	cv::findContours(green_mask, green_contours, green_hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+	findCentroid(green_contours, &hand->x, &hand->y);
 }
 
 
@@ -211,12 +279,19 @@ HandTracker::~HandTracker()
 {
 }
 
-//startTracking
+/*//startTracking
 //Starts a tracking thread
 //Param @ callback: it calls the callback with an array of hands
 void HandTracker::startTracking(const std::function<void(std::array<hand, HANDS_AMOUNT>)>& callback)
 {
 	callback(hands);
 	std::thread tracking_thread(&HandTracker::trackByColor, this, callback);
+	tracking_thread.detach();
+}*/
+
+void HandTracker::startTracking(const std::function<void(std::array<hand, HANDS_AMOUNT>)>& callback)
+{
+	callback(hands);
+	std::thread tracking_thread(&HandTracker::track, this, callback);
 	tracking_thread.detach();
 }
